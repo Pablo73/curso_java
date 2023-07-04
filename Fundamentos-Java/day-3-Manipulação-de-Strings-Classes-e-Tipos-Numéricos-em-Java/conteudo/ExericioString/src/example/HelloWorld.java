1159d3690b64628a144c2d70ff57266bdcd882af
Pablo Dominguez <pasaxo@gmail.com>
2023-07-04 18:45:33 -0300

    conteudo do dia

---
+++ Fundamentos-Java/day-3-Manipulação-de-Strings-Classes-e-Tipos-Numéricos-em-Java/conteudo/ExericioString/src/example/HelloWorld.java
@@ -0,0 +1,17 @@
+package example;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+
+    String saudacao = "Hello, world!";
+
+    System.out.println(saudacao);
+    System.out.println("Primeira Linha");
+    System.out.println("Segunda Linha");
+    System.out.print("Primeira Linha");
+    System.out.print("Será impresso também na primeira linha");
+
+  }
+
+}
