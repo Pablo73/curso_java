cb75ed6fad0183aa3ff5f5e17fd173aeca9b758b
Pablo Dominguez <pasaxo@gmail.com>
2023-07-07 16:42:45 -0300

    conteudo polimorfismo Override e Overload

---
+++ Orientacao-a-objetos/day-2-Polimorfismo-Interfaces-e-Classes-Abstratas/conteudo/src/polimorfismoSobrescrita/PolimorfismoCarro.java
@@ -0,0 +1,11 @@
+package polimorfismoSobrescrita;
+
+public class PolimorfismoCarro extends PolimorfismoVeiculo {
+
+  private String modelo;
+
+  public void acelerar() {
+    System.out.println("Método acelerar com a mão.");
+  }
+
+}
---
+++ Orientacao-a-objetos/day-2-Polimorfismo-Interfaces-e-Classes-Abstratas/conteudo/src/polimorfismoSobrescrita/PolimorfismoVeiculo.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package polimorfismoSobrescrita;
+
+public abstract class PolimorfismoVeiculo {
+
+  public void abastecer() {
+    System.out.println("Método abastecer da classe Veiculo.");
+  }
+
+  public abstract void acelerar();
+
+}
