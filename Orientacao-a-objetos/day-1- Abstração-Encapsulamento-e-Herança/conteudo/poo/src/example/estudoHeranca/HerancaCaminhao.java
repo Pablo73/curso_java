c34e37d82875840b243ad63890acae1eaa43d3b8
Pablo Dominguez <pasaxo@gmail.com>
2023-07-07 09:22:54 -0300

    final do conteudo modificador de acesso protected e default

---
+++ Orientacao-a-objetos/day-1- Abstração-Encapsulamento-e-Herança/conteudo/poo/src/example/estudoHeranca/HerancaCaminhao.java
@@ -0,0 +1,16 @@
+package example.estudoHeranca;
+
+public class HerancaCaminhao extends HeracaVeiculo {
+
+  public HerancaCaminhao() {
+    super.placa = placa;
+    super.motorista = motorista;
+    super.cor = cor;
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
